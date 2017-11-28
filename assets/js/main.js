c25caa4dfb4630c9b4be3dfb1aba75fc8faea11f
Oliver Adria <oliver@adria.io>
2017-11-28 18:54:34 +0100

    Basic Homepage setup with first data

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+require('../css/global.scss');
+
+import $ from 'jquery';
+import Vue from 'vue/dist/vue.esm';
+import 'bootstrap/dist/js/bootstrap.min';
+
+global.$ = $;
+// window.$ = $;
+
+global.Vue = Vue;
---
+++ webpack.config.js
@@ -0,0 +1,69 @@
+// webpack.config.js
+var Encore = require('@symfony/webpack-encore');
+
+Encore
+    // the project directory where all compiled assets will be stored
+    .setOutputPath('public/build/')
+
+    // the public path used by the web server to access the previous directory
+    .setPublicPath('/build')
+
+    // will create public/build/app.js and public/build/app.css
+    .addEntry('app', './assets/js/main.js')
+
+    // allow sass/scss files to be processed
+    .enableSassLoader()
+
+    // allow legacy applications to use $/jQuery as a global variable
+    .autoProvidejQuery()
+
+    .enableSourceMaps(!Encore.isProduction())
+
+    // empty the outputPath dir before each build
+    .cleanupOutputBeforeBuild()
+
+    .enableVueLoader()
+
+    .autoProvideVariables({
+        $: 'jquery',
+        jQuery: 'jquery',
+        'window.jQuery': 'jquery',
+        Popper: ['popper.js', 'default'],
+    })
+
+    // show OS notifications when builds finish/fail
+    .enableBuildNotifications()
+
+    .addLoader(
+        {
+            test: /\.woff(\?v=\d+\.\d+\.\d+)?$/,
+            loader: "url-loader?limit=10000&mimetype=application/font-woff"
+        },
+        {
+            test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/,
+            loader: "url-loader?limit=10000&mimetype=application/font-woff"
+        },
+        {
+            test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+            loader: "url-loader?limit=10000&mimetype=application/octet-stream"
+        },
+        {
+            test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+            loader: "file"
+        },
+        {
+            test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+            loader: "url-loader?limit=10000&mimetype=image/svg+xml"
+        }
+    )
+
+    // create hashed filenames (e.g. app.abc123.css)
+    // .enableVersioning()
+;
+
+// Add a watcher
+var config = Encore.getWebpackConfig();
+config.watchOptions = { poll: true, ignored: /node_modules/ };
+
+// export the final configuration
+module.exports = config;
